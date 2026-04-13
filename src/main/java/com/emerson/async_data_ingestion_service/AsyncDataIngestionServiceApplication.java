014fca9721b29fe3726b50c3661376fb231677da
Emerson-feral <emerson_feral@hotmail.com>
2026-04-13 20:34:14 -0300

    Git init

---
+++ src/main/java/com/emerson/async_data_ingestion_service/AsyncDataIngestionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.emerson.async_data_ingestion_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsyncDataIngestionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsyncDataIngestionServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emerson/async_data_ingestion_service/AsyncDataIngestionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emerson.async_data_ingestion_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsyncDataIngestionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
